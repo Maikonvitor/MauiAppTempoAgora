174787235bb981874bf5a1a1965620f6833483a0
vitorhhk8 <maikonvitor450@gmail.com>
2026-04-05 19:32:27 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppTempoAgora/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace MauiAppTempoAgora
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using MauiAppTempoAgora.Models;
+using MauiAppTempoAgora.Services;
+
+namespace MauiAppTempoAgora
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Connectivity.Current.NetworkAccess != NetworkAccess.Internet)
+                {
+                    await DisplayAlert("Sem conexão", "Você está sem conexão com a internet.", "OK");
+                    return;
+                }
+
+                if(!string.IsNullOrEmpty(txt_cidade.Text))
+                {
+                    Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
+
+                    if(t != null) 
+                    {
+                        string dados_previsao = "";
+
+                        dados_previsao = $"Latitude: {t.lat} \n" +
+                                         $"Longitude: {t.lon} \n" +
+                                         $"Descrição: {t.description} \n" +
+                                         $"Nascer do Sol: {t.sunrise} \n" +
+                                         $"Por do Sol: {t.sunset} \n" +
+                                         $"Temp Máx: {t.temp_max} \n" +
+                                         $"Temp Min: {t.temp_min} \n" +
+                                         $"Vento: {t.speed} m/s \n" +
+                                         $"Visibilidade: {t.visibility} m\n";
+
+                        lbl_res.Text = dados_previsao;
+
+                    } else
+                    {
+
+                        lbl_res.Text = "Sem dados de Previsão";
+                    }
+
+                } else
+                {
+                    lbl_res.Text = "Preencha a cidade.";
+                }
+
+            } catch (InvalidOperationException ex)
+            {
+                lbl_res.Text = ex.Message;
+            } catch(Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+    }
+
+}
